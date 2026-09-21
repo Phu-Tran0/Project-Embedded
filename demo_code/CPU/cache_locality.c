ab0cd0773d93ab2ee38746c8ff60e858842de03f
Phu-Tran0 <t.giaphu69@gmail.com>
2026-09-21 17:11:42 +0700

    Update cache_locality.c

--- demo_code/CPU/cache_locality.c
+++ demo_code/CPU/cache_locality.c
@@ -4,6 +4,66 @@
 
 #define SIZE 100000000
 
+int array[SIZE];
+
+double get_time()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec / 1e9;
+}
+
+long long sequential_access()
+{
+    long long sum = 0;
+
+    for(long long i = 0; i < SIZE; i++)
+        sum += array[i];
+
+    return sum;
+}
+
+long long random_access()
+{
+    long long sum = 0;
+
+    for(long long i = 0; i < SIZE; i++)
+    {
+        long long index = (i * 7919) % SIZE;
+        sum += array[index];
+    }
+
+    return sum;
+}
+
+int main()
+{
+    for(long long i = 0; i < SIZE; i++)
+        array[i] = 1;
+
+    double start, end;
+
+    start = get_time();
+    printf("Sequential sum: %lld\n", sequential_access());
+    end = get_time();
+
+    printf("Sequential time: %.6f seconds\n\n",
+           end - start);
+
+    start = get_time();
+    printf("Random sum: %lld\n", random_access());
+    end = get_time();
+
+    printf("Random time: %.6f seconds\n",
+           end - start);
+
+    return 0;
+}#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define SIZE 100000000
+
 
 int array[SIZE];
 
